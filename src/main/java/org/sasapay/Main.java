bd9a43e7b86ce6326bb42a6df44a954d48ff455a
ErickMusau <erickmusau.mutisya@gmail.com>
2023-02-10 10:45:43 +0300

    thread fixs

--- src/main/java/org/sasapay/Main.java
+++ src/main/java/org/sasapay/Main.java
@@ -1,12 +1,10 @@
 package org.sasapay;
-
 import com.github.tsohr.JSONObject;
-
 public class Main {
     public static void main(String[] args)  {
 
-//        String clientId = "JKIYiwGW1C3x9OAozWpYrRYE7dIHFELkOPcYjHfQwA";
-//        String clientSecret = "PP7HXSTbS9hxXrPNkheB0WuUqAfhlqtzPyf6HFFcMa2S8pCNGHmTYFKxn6vrH02wL0PFM4qnrgXUa5WrLMWTjfyFJY3E15aGfCHKGHIF9LzoJatKoHf85JVunAiWY6HYD";
+//        String clientId = "JKIYiwGW1C3x9OAozWpYrRYE7dIHFELkOPcYjHfQwAT";
+//        String clientSecret = "PP7HXSTbS9hxXrPNkheB0WuUqYAfhlqtzPyf6HFFcMa2S8pCNGHmTYFKxn6vrH02wL0PFM4qnrgXUa5WrLMWTjfyFJY3E15aGfCHKGHIF9LzoJatKoHf85JVunAiWY6HYD";
 //
 //       JSONObject json =new Waas().getWaasAccessToken( clientId, clientSecret);
 //
